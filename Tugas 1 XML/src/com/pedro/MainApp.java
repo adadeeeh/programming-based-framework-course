2dd51304153e4e70a8434ed195920f4e3adb5050
Faturrahman <faturrahmanmakruf@gmail.com>
2019-03-18 04:02:56 +0700

    xml IoC done

---
+++ Tugas 1 XML/src/com/pedro/MainApp.java
@@ -0,0 +1,19 @@
+package com.pedro;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		//IoC
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("creature.xml");
+		
+		Orc orc1 = context.getBean("orc1", Orc.class);
+		orc1.getEat();
+		System.out.println("Orc1 name: " + ((Orc)orc1).getName());
+		System.out.println("Orc1 age: " + ((Orc)orc1).getAge());
+		System.out.println("Orc1 speed: " + ((Orc)orc1).getSpeed());
+		context.close();
+	}
+
+}
---
+++ Tugas 1 XML/src/com/pedro/Orc.java
@@ -0,0 +1,47 @@
+package com.pedro;
+
+public class Orc implements Creature {
+	private String name;
+	private int age;
+	private String speed;
+	
+	public Orc() {
+	}
+
+	public Orc(String name, int age, String speed) {
+		this.name = name;
+		this.age = age;
+		this.speed = speed;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(String speed) {
+		this.speed = speed;
+	}
+
+	@Override
+	public void getEat() {
+		// TODO Auto-generated method stub
+		System.out.println("I like this food!");
+	}
+
+}
